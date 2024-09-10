8151d2b02da1e727edb88bc25a1215243fb30fee
Panurat <paunavt5@gmail.com>
2024-09-10 14:34:58 +0700

    rust basic

---
+++ src/var.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let x: i32 = 5; // i32 เป็นประเภทข้อมูลที่ใช้สำหรับจำนวนเต็ม 32-bit
+    let y = 10; // Rust สามารถอนุมานประเภทข้อมูลได้
+    let sum = x + y;
+    println!("The sum of {} and {} is {}", x, y, sum);
+}
